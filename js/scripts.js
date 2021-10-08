a10936d6e94d3dae0535b0f5ef1d546f579db4b2
Mega-117 <enricosaian@hotmail.it>
2021-10-08 20:44:05 +0200

    inserito reload page

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,50 @@
-let chilometriViaggio = prompt("inserisci i km del viaggio")
-let prezzoViaggio = chilometriViaggio * 0.21
+let chilometriViaggio = prompt("inserisci i km del viaggio");
+let prezzoViaggio = chilometriViaggio * 0.21;
 
 if (chilometriViaggio === null || chilometriViaggio <= 0) {
-    alert("km non inseriti riprova")
+    alert("km non inseriti riprova");
+    location.reload();
 }
 
 else if (isNaN(chilometriViaggio)) {
-    alert("Valore chilometri non valido riprova")
+    alert("Valore chilometri non valido riprova");
 } else {
-    console.log("viaggio di " + chilometriViaggio + " km")
-    let eta = prompt("età")
-    console.log("anni " + eta)
+    console.log("viaggio di " + chilometriViaggio + " km");
+    let eta = prompt("età");
+    console.log("anni " + eta);
 
     if (eta === null || eta == "") {
-        alert("età non inserita riprova")
+        alert("età non inserita riprova");
+        location.reload();
     }
 
     else if (isNaN(eta)) {
-        alert("Valore età non valido riprova")
+        alert("Valore età non valido riprova");
     }
 
     else if (eta < 18) {
-        console.log("Minorenne")
-        alert("Sei minorenne. Hai uno sconto del 20%")
-        let scontoMinori = ((prezzoViaggio * 20) / 100)
-        let prezzoMinori = prezzoViaggio - scontoMinori
-        prezzoMinori = prezzoMinori.toFixed(2)
-        console.log("il biglietto costa " + prezzoMinori)
-        alert("Il biglietti ti viene " + prezzoMinori)
+        console.log("Minorenne");
+        alert("Sei minorenne. Hai uno sconto del 20%");
+        let scontoMinori = ((prezzoViaggio * 20) / 100);
+        let prezzoMinori = prezzoViaggio - scontoMinori;
+        prezzoMinori = prezzoMinori.toFixed(2);
+        console.log("il biglietto costa " + prezzoMinori);
+        alert("Il biglietti ti viene " + prezzoMinori);
     }
     else if (eta >= 18 && eta <= 65) {
-        console.log("Maggiorenne")
-        alert("Sei maggirenne")
-        prezzoViaggio = prezzoViaggio.toFixed(2)
-        console.log("il biglietto costa " + prezzoViaggio)
-        alert("Il biglietto viene " + prezzoViaggio)
+        console.log("Maggiorenne");
+        alert("Sei maggirenne");
+        prezzoViaggio = prezzoViaggio.toFixed(2);
+        console.log("il biglietto costa " + prezzoViaggio);
+        alert("Il biglietto viene " + prezzoViaggio);
     }
     else if (eta > 65) {
-        console.log("Over 65")
-        alert("Complimenti hai più di 64 anni hai diritto a uno sconto del 40%")
-        let scontoOver = ((prezzoViaggio * 40) / 100)
-        let prezzoOver = prezzoViaggio - scontoOver
-        prezzoOver = prezzoOver.toFixed(2)
-        console.log("il biglietto costa " + prezzoOver)
-        alert("Il tuo biglietto viene " + prezzoOver)
+        console.log("Over 65");
+        alert("Complimenti hai più di 64 anni hai diritto a uno sconto del 40%");
+        let scontoOver = ((prezzoViaggio * 40) / 100);
+        let prezzoOver = prezzoViaggio - scontoOver;
+        prezzoOver = prezzoOver.toFixed(2);
+        console.log("il biglietto costa " + prezzoOver);
+        alert("Il tuo biglietto viene " + prezzoOver);
     }
 }
